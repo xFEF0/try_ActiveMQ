0037519f4c75b113bf27d785511d9ddc46dc9560
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-05 14:54:15 -0300

    Initial commit

---
+++ src/main/java/com/xfef0/tryActiveMQ/TryActiveMqApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.tryActiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryActiveMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfef0/tryActiveMQ/TryActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfef0.tryActiveMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryActiveMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
